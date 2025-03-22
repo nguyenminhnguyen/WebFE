5468c3f82c2c669d18efd8e11460024b6268b8b7
nguyenminhnguyen <23521046@gm.uit.edu.vn>
2025-03-22 12:54:53 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./components/navbar"
+import Search from "./components/search"
+import './App.css'
+function App() {  
+  return (
+    <div>
+      <NavBar/>
+      <Search/>
+    </div>
+  )
+}
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function NavBar(){
+    return (
+        <nav className="bg-white p-4 flex justify-between ">
+            <div className="flex space-x-4">
+            <h1 className="text-2xl font-bold" >Freelancer AI</h1>
+            <ul className="flex space-x-4">
+                <li>
+                    <a href="/" className="text-black hover:text-green-700">Trang chủ</a>
+                </li>
+                <li>
+                    <a href="/project" className="text-black hover:text-green-700">Dự án</a>
+                </li>
+                <li>
+                    <a href="/freelancer" className="text-black hover:text-green-700">Freelancer</a>
+                </li>
+            </ul>
+            </div>
+            <div className="flex space-x-4">
+                    <a href="/login" className="text-black py-2 hover:text-green-700">Đăng nhập</a>
+                    <a href="/signup" className="text-white bg-green-700 px-4 py-2 rounded-xl hover:bg-green-600">Đăng ký</a>
+            </div>
+        </nav>
+    )
+}
+export default NavBar
